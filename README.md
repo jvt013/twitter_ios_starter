95231c1edefa7ef6ed60b1f594853d7c28a46591
Jenny Trinh <jvt013@ucsd.edu>
2020-11-10 19:19:36 -0800

    Retweet, Favorite, Tweet Features

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Project 3 - *Twitter*
+# Project 3 - Twitter
 
-**Twitter** is a basic twitter app to read your tweets.
+Twitter is a basic twitter app to read your tweets.
 
 Time spent: **10** hours spent in total
 
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,7 +14,67 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    @IBOutlet weak var favButton: UIButton!
     
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweeting: \(error)")
+        })
+//        else {
+//            TwitterAPICaller.client?.unretweet(tweetId: tweetId, success: {
+//                self.setRetweeted(false)
+//            }, failure: { (error) in
+//                print("Unretweet did not succeed: \(error)")
+//            })
+//
+//        }
+    }
+    
+    @IBAction func favoriteTweet(_ sender:Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        
+        }
+    }
+    
+    var retweeted:Bool = false
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    //set favorite icon to red icon when favorited
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if(favorited) {
+            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+    }
+    //set retweet icon to green when retweeted
+    func setRetweeted(_ isRetweeted:Bool) {
+        retweeted = isRetweeted
+        if(retweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+        
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -27,3 +87,4 @@ class TweetCellTableViewCell: UITableViewCell {
     }
 
 }
+
